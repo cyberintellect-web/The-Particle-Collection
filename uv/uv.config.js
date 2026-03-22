e738cc6a1219a3363aeb2a8c1ddae566db92da81
cyberintellect-web <zahccalamartdi@gmail.com>
2026-03-21 22:15:22 -0400

    er

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/service/',
-    bare: 'https://vault-bare.vercel.app/', // Updated to your server
+    prefix: '/core/',
+    bare: 'https://vault-bare.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: '/uv.sw.js',
+    sw: '/uv.sw.js', // This points to the one you moved to the root
 };
